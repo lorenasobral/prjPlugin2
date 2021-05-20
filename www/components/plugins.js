3c21f19ac78fa5bec20caef836ac87951be59f59
lorenasobral <sobral.abx@gmail.com>
2021-05-21 06:33:47 +0900

    Project's Initial Commit

---
+++ www/components/plugins.js
@@ -0,0 +1,15 @@
+
+
+$(document).on("click","#camera", function() {
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+});
