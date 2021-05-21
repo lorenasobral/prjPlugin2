bcec4809154b08c139677a659c853506425f8997
lorenasobral <79669642+lorenasobral@users.noreply.github.com>
2021-05-21 10:56:10 -0300

    plugin_scanner

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -13,3 +13,31 @@ function onFail(message) {
     alert('Failed because: ' + message);
 }
 });
+
+$(document).on("click","#codigo", function() {
+   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, 
+          showFlipCameraButton : true, 
+          showTorchButton : true, 
+          torchOn: true, 
+          saveHistory: true, 
+          prompt : "Place a barcode inside the scan area", 
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417, CODE39",
+          orientation : "landscape", 
+          disableAnimations : true, 
+          disableSuccessBeep: false 
+      }
+   );
+
+});
